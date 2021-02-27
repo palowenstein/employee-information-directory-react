e5d9329a614816e6a42e047e71b4398e5cf4dcd6
abbotkinneydude <pa@soundtrackspecialist.com>
2021-02-26 17:15:27 -0800

    main application file 'Employee Retriever' in src/assembly directory

---
+++ src/assembly/EmployeeRetriever.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import SearchForm from "../components/SearchForm";
+import SearchResults from "../components/SearchResults";
+
+class EmployeeRetriever extends Component {
+  state = {
+    search: "",
+    ascending: "",
+    location: "",
+    results: []
+  };
+
+  // When the component mounts, load available employees to be displayed
+  componentDidMount() { this.loadEmployees(); }
+
+  handleInputChange = (event) => {
+    event.preventDefault();
+    this.setState({ search: event.target.value.toLowerCase() });
+  };
+
+  // Alphabetical Sorting
+  handleSort = (event) => {
+    event.preventDefault();
+    const { ascending } = this.state;
+    // Ascending
+    if (ascending === false || ascending === "") { this.handleSortAscending(); }
+    // Descending
+    else if (ascending === true) { this.handleSortDescending(); }
+    // Place    
+    else if (ascending === true) { this.handleSortLocation(); }
+    return;
+  };
+
+  // Ascending Sorting
+  handleSortAscending = () => {
+    this.setState({ results: this.state.results.sort((a, b) => a.name.last.localeCompare(b.name.last) ),
+      ascending: true });
+  };
+
+  // Descending Sorting
+  handleSortDescending = () => {
+    this.setState({ results: this.state.results.sort((a, b) => b.name.last.localeCompare(a.name.last) ),
+      ascending: false });
+  };
+
+  // Employee Information
+  loadEmployees = () => {
+    API.getStockEmployees()
+      .then((res) => this.setState({ results: res.data.results, }) )
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    const filter = this.state.results.filter((results) =>
+      results.name.last.toLowerCase().includes(this.state.search) // .toLowerCase() function needed for search query to function properly
+    );
+
+    return (
+        <div>
+          <SearchForm handleSort={this.handleSort} handleInputChange={this.handleInputChange}></SearchForm>
+          <SearchResults results={filter}></SearchResults>
+        </div>
+    );
+  }
+}
+
+export default EmployeeRetriever;
---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import backgroundImage from "./purple-layer.jpg";
+
+const SearchForm = (props) => {
+  return (
+    <div style={{
+      textAlign: "center",
+      margin: "0 auto",
+      verticalAlign: "middle",
+      backgroundImage: `url(${backgroundImage})`,
+      width: "100%",
+      maxWidth: 1200,
+      height: "100%",
+      minHeight: 300,
+      marginTop: 20,
+      backgroundColor: "#9228ae"
+      }}
+      >
+    <h1 style={{
+      color: "#ffffff",
+      fontWeight: 800,
+      paddingTop: 20}}
+      >
+      Employee Retriever
+    </h1>
+    <h6 style={{
+      color: "#ffffff",
+      fontWeight: 600}}
+      >
+      Enter full or partial name to retrieve employee or sort alphabetically.
+    </h6>
+    
+    <form className="search">
+      <section className="form-group">
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="search"
+          list="names"
+          type="text"
+          className="form-control"
+          placeholder="Employee Last Name"
+          id="name"
+          style={{marginTop: 0, fontSize: "1em"}}
+        />
+        <button
+          type="submit"
+          onClick={props.handleSort}
+          style={{backgroundColor: "#ffffff", color: "#42147e", fontWeight: 600, fontSize: "1em"}}
+        >
+          Alphabetical Sorting
+        </button>
+      </section>
+    </form>
+    </div>
+  );
+}
+
+export default SearchForm;
